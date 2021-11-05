f39e20540e3927be09b2d6ad7576de20622fea36
Henrik Nilsson <henrik@lhjnilsson.com>
2021-11-05 22:32:44 +0100

    to make it public

--- setup.py
+++ setup.py
@@ -9,11 +9,16 @@ with pathlib.Path("requirements.txt").open() as requirements_txt:
 
 setup(
     name="foreverbull-core",
-    version="0.0.12",
+    version="0.0.13",
     description="Core Python functionality for Foreverbull",
     author="Henrik Nilsson",
     author_email="henrik@lhjnilsson.com",
     packages=find_packages("src"),
     package_dir={"": "src/"},
     install_requires=install_requires,
+    python_requires=">=3.6",
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
 )
