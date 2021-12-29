6a724e9899614931b463c2415e89af044b3145d6
Henrik Nilsson <henrik@lhjnilsson.com>
2021-12-29 21:28:46 +0100

    better use of text information when working with cli

--- src/foreverbull_core/http/service.py
+++ src/foreverbull_core/http/service.py
@@ -17,44 +17,63 @@ class Service:
     def list(self) -> List[service.Service]:
         rsp = self.session.get(f"http://{self.host}/api/v1/services")
         if not rsp.ok:
-            raise RequestError(f"get call /services gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""get call /services gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return [service.Service(**s) for s in rsp.json()]
 
     def create(self, service: service.Service) -> service.Service:
         rsp = self.session.post(f"http://{self.host}/api/v1/services", json=service.dict())
         if not rsp.ok:
-            raise RequestError(f"post call /services gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""post call /services gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return service.update_fields(rsp.json())
 
     def get(self, service_id: str) -> service.Service:
         rsp = self.session.get(f"http://{self.host}/api/v1/services/{service_id}")
         if not rsp.ok:
-            raise RequestError(f"get call /services/{service_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""get call /services/{service_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return service.Service(**rsp.json())
 
     def update(self, service_id: str, service: service.Service) -> service.Service:
         rsp = self.session.put(f"http://{self.host}/api/v1/services/{service_id}", json=service.dict())
         if not rsp.ok:
-            raise RequestError(f"put call /services/{service_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""put call /services/{service_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return service.update_fields(rsp.json())
 
     def delete(self, service_id: str) -> None:
         rsp = self.session.delete(f"http://{self.host}/api/v1/services/{service_id}")
         if not rsp.ok:
-            raise RequestError(f"delete call /services/{service_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""delete call /services/{service_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return None
 
     def list_instances(self, service_id: str) -> List[service.Instance]:
         rsp = self.session.get(f"http://{self.host}/api/v1/services/{service_id}/instances")
         if not rsp.ok:
-            raise RequestError(f"get call /services/{service_id}/instances gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""get call /services/{service_id}/instances gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return [service.Instance(**i) for i in rsp.json()]
 
     def get_instance(self, service_id: str, instance_id: str) -> service.Instance:
         rsp = self.session.get(f"http://{self.host}/api/v1/services/{service_id}/instances/{instance_id}")
         if not rsp.ok:
             raise RequestError(
-                f"get call /services/{service_id}/instances/{instance_id} gave bad return code: {rsp.status_code}"
+                f"""get call /services/{service_id}/instances/{instance_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
             )
         return service.Instance(**rsp.json())
 
@@ -65,13 +84,17 @@ class Service:
         )
         if not rsp.ok:
             code = rsp.status_code  # to mitigate next line too long
-            raise RequestError(f"get call /services/{ins.service_id}/instances/{ins.id} gave bad return code: {code}")
+            raise RequestError(
+                f"""get call /services/{ins.service_id}/instances/{ins.id} gave bad return code: {code}
+            Text: {rsp.text}"""
+            )
         return ins.update_fields(rsp.json())
 
     def delete_instance(self, service_id: str, instance_id: str) -> None:
         rsp = self.session.delete(f"http://{self.host}/api/v1/services/{service_id}/instances/{instance_id}")
         if not rsp.ok:
             raise RequestError(
-                f"delete call /services/{service_id}/instances/{instance_id} gave bad return code: {rsp.status_code}"
+                f"""delete call /services/{service_id}/instances/{instance_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
             )
         return None
--- src/foreverbull_core/http/worker.py
+++ src/foreverbull_core/http/worker.py
@@ -16,29 +16,44 @@ class Worker:
     def list(self) -> List[worker.Config]:
         rsp = self.session.get(f"http://{self.host}/api/v1/workers")
         if not rsp.ok:
-            raise RequestError(f"get call /workers gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""get call /workers gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return [worker.Config(**w) for w in rsp.json()]
 
     def create(self, worker: worker.Config) -> worker.Config:
         rsp = self.session.post(f"http://{self.host}/api/v1/workers", json=worker.json())
         if not rsp.ok:
-            raise RequestError(f"post call /workers gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""post call /workers gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return worker.update_fields(rsp.json())
 
     def get(self, worker_id: int) -> worker.Config:
         rsp = self.session.get(f"http://{self.host}/api/v1/workers/{worker_id}")
         if not rsp.ok:
-            raise RequestError(f"get call /workers/{worker_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""get call /workers/{worker_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return worker.Config(**rsp.json())
 
     def update(self, worker_id: int, worker: worker.Config) -> worker.Config:
         rsp = self.session.put(f"http://{self.host}/api/v1/workers/{worker_id}", json=worker.json())
         if not rsp.ok:
-            raise RequestError(f"put call /workers/{worker_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""put call /workers/{worker_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return worker.update_fields(rsp.json())
 
     def delete(self, worker_id: int) -> None:
         rsp = self.session.delete(f"http://{self.host}/api/v1/workers/{worker_id}")
         if not rsp.ok:
-            raise RequestError(f"delete call /workers/{worker_id} gave bad return code: {rsp.status_code}")
+            raise RequestError(
+                f"""delete call /workers/{worker_id} gave bad return code: {rsp.status_code}
+            Text: {rsp.text}"""
+            )
         return None
